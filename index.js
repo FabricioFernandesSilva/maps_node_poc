16a73cd16beb3a0f276ebcf46510cd4cbe664d35
Fabricio <fabriciofs.comp@gmail.com>
2016-10-07 17:52:31 -0300

    project starting point

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require ('express');
+var app =   express ();
+var organizations = {''};
+app.get('/', function(req, res){
+  res.end();
+});
+
+app.listen(3000, function(){
+    console.log("listening on port 3000")
+});
