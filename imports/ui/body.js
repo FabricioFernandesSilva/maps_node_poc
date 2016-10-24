f05f07bb48c1837f53f87588dc1cf41d82741d58
Fabricio <fabriciofs.comp@gmail.com>
2016-10-24 19:43:43 -0200

    trying to put the map to work]

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,12 @@
 import { Template } from "meteor/templating";
 import './body.html';
-  
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+
+template.body.map({
+  var mapboxgl = require('mapbox-gl');
+  mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FsZiIsImEiOiJjaXVvaHgwY3AwMWViMzByemNocm03cjZhIn0.Fme5YNDuF7wlTXMykIFNTw';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9'
+  });
+});
